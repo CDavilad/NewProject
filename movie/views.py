9aba796039f74f477d31f9862211939908735f17
Cdavilad <Cddavilag@eafit.edu.co>
2022-02-10 11:06:01 -0500

    Cambios en función home

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>En guardia Penitente!</h1>')
+    return HttpResponse('<h1>Welcome to the homepage Cristian</h1>')
 
 # Create your views here.
