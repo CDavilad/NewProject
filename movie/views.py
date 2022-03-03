9b6a1fde9996e0e9c2e4ea8326a25c498de70138
Cdavilad <Cddavilag@eafit.edu.co>
2022-03-03 10:09:07 -0500

    Formulario de búsqueda

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('<h1>Welcome to the homepage Cristian</h1>')
-
 # Create your views here.
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    return render(request,'home.html', {'searchTerm': searchTerm})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
